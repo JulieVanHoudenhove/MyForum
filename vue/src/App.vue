77d83b7d19d37a279f15cb67ee8c4b23a6f48d0b
Julie Van Houdenhove <100696436+JulieVanHoudenhove@users.noreply.github.com>
2023-04-28 09:55:01 +0200

    compte/stats

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -19,7 +19,7 @@
             <RouterLink class="block text-black font-bold text-3xl" to="/"><span class="text-vert text-4xl">F</span>orum</RouterLink>
             <ul class="flex flex-row justify-around w-6/12">
                 <li><RouterLink class="nav_li_a" to="/">Accueil</RouterLink></li>
-                <li v-if="current"><RouterLink class="nav_li_a" to="/statistiques">Statistiques</RouterLink></li>
+                <li v-if="current"><RouterLink class="nav_li_a" :to="'/statistiques/' + current.id">Statistiques</RouterLink></li>
                 <li v-if="current"><RouterLink class="nav_li_a" :to="'/compte/' + current.id">Compte</RouterLink></li>
                 <li v-if="current"><RouterLink class="nav_li_a" to="/deconnexion">Déconnexion</RouterLink></li>
                 <li v-if="!current"><RouterLink class="nav_li_a" to="/inscription">Inscription</RouterLink></li>
